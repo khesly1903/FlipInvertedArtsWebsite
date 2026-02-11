5596f6f4a7131fc08b9a70e9640f393bb055a63c
khesly1903 <kayaberkay1626@gmail.com>
2026-02-11 19:47:18 +0200

    data: shop items added

--- src/data/shop.js
+++ src/data/shop.js
@@ -1,14 +1,43 @@
-import mat_video from "../assets/shop/mat/mat_video.mp4";
-import mat_black_1 from "../assets/shop/mat/mat_black_1.webp";
-import mat_black_2 from "../assets/shop/mat/mat_black_2.webp";
-import mat_black_3 from "../assets/shop/mat/mat_black_3.webp";
-import mat_black_4 from "../assets/shop/mat/mat_black_4.webp";
-import mat_black_5 from "../assets/shop/mat/mat_black_5.webp";
-import mat_black_6 from "../assets/shop/mat/mat_black_6.webp";
-import mat_black_7 from "../assets/shop/mat/mat_black_7.webp";
-import mat_black_8 from "../assets/shop/mat/mat_black_8.webp";
-import mat_black_9 from "../assets/shop/mat/mat_black_9.webp";
-import mat_blue_1 from "../assets/shop/mat/mat_blue_1.webp";
+// Mats
+import mat_video from "../assets/shop/mats/mat_video.mp4";
+import mat_black_1 from "../assets/shop/mats/mat_black_1.webp";
+import mat_black_2 from "../assets/shop/mats/mat_black_2.webp";
+import mat_black_3 from "../assets/shop/mats/mat_black_3.webp";
+import mat_black_4 from "../assets/shop/mats/mat_black_4.webp";
+import mat_black_5 from "../assets/shop/mats/mat_black_5.webp";
+import mat_black_6 from "../assets/shop/mats/mat_black_6.webp";
+import mat_black_7 from "../assets/shop/mats/mat_black_7.webp";
+import mat_black_8 from "../assets/shop/mats/mat_black_8.webp";
+import mat_black_9 from "../assets/shop/mats/mat_black_9.webp";
+import mat_blue_1 from "../assets/shop/mats/mat_blue_1.webp";
+
+// Pants
+import pants_adult_1 from "../assets/shop/pants/pants_adult_1.webp";
+import pants_adult_2 from "../assets/shop/pants/pants_adult_2.webp";
+import pants_adult_3 from "../assets/shop/pants/pants_adult_3.webp";
+import pants_adult_4 from "../assets/shop/pants/pants_adult_4.webp";
+
+// Crop Set Shorts
+import crop_set_shorts_1 from "../assets/shop/crop_set_shorts/crop_set_shorts_1.webp";
+import crop_set_shorts_2 from "../assets/shop/crop_set_shorts/crop_set_shorts_2.webp";
+import crop_set_shorts_3 from "../assets/shop/crop_set_shorts/crop_set_short_3.webp";
+import crop_set_shorts_4 from "../assets/shop/crop_set_shorts/crop_set_shorts_4.webp";
+
+// Long Sleeves
+import long_sleeve_kids_1 from "../assets/shop/long_sleeves_kids/long_sleeve_kids_1.webp";
+import long_sleeve_kids_2 from "../assets/shop/long_sleeves_kids/long_sleeve_kids_2.webp";
+import long_sleeve_kids_3 from "../assets/shop/long_sleeves_kids/long_sleeve_kids_3.webp";
+import long_sleeve_kids_4 from "../assets/shop/long_sleeves_kids/long_sleeve_kids_4.webp";
+
+// Short Sleeves
+import short_sleeve_1 from "../assets/shop/short_sleeves/short_sleeve_1.webp";
+import short_sleeve_2 from "../assets/shop/short_sleeves/short_sleeve_2.webp";
+
+// Tank Tops
+import tank_top_male_1 from "../assets/shop/tank_tops/tank_top_male_1.webp";
+import tank_top_male_2 from "../assets/shop/tank_tops/tank_top_male_2.webp";
+import tank_top_male_3 from "../assets/shop/tank_tops/tank_top_male_3.webp";
+import tank_top_male_4 from "../assets/shop/tank_tops/tank_top_male_4.webp";
 
 export const shopCategories = [
   { id: "long-sleeve", name: "shop.categories.long-sleeve" },
@@ -96,7 +125,12 @@ export const shopItems = [
     availability: "available",
     tags: ["apparel", "tops", "long-sleeve"],
     slug: "long-sleeve-top",
-    images: [{ src: mat_black_1, alt: "Long Sleeve" }],
+    images: [
+      { src: long_sleeve_kids_1, alt: "Long Sleeve Kids 1" },
+      { src: long_sleeve_kids_2, alt: "Long Sleeve Kids 2" },
+      { src: long_sleeve_kids_3, alt: "Long Sleeve Kids 3" },
+      { src: long_sleeve_kids_4, alt: "Long Sleeve Kids 4" },
+    ],
     variants: [],
     details: [
       "shop.long-sleeve.details.1",
@@ -115,7 +149,10 @@ export const shopItems = [
     availability: "available",
     tags: ["apparel", "tops", "short-sleeve"],
     slug: "short-sleeve-tee",
-    images: [{ src: mat_black_1, alt: "Short Sleeve" }],
+    images: [
+      { src: short_sleeve_1, alt: "Short Sleeve 1" },
+      { src: short_sleeve_2, alt: "Short Sleeve 2" },
+    ],
     variants: [],
     details: [
       "shop.short-sleeve.details.1",
@@ -134,7 +171,12 @@ export const shopItems = [
     availability: "available",
     tags: ["apparel", "tops", "tank-tops"],
     slug: "training-tank-top",
-    images: [{ src: mat_black_1, alt: "Tank Top" }],
+    images: [
+      { src: tank_top_male_1, alt: "Tank Top Male 1" },
+      { src: tank_top_male_2, alt: "Tank Top Male 2" },
+      { src: tank_top_male_3, alt: "Tank Top Male 3" },
+      { src: tank_top_male_4, alt: "Tank Top Male 4" },
+    ],
     variants: [],
     details: [
       "shop.tank-tops.details.1",
@@ -153,7 +195,12 @@ export const shopItems = [
     availability: "available",
     tags: ["apparel", "bottoms", "pants"],
     slug: "gymnastics-pants",
-    images: [{ src: mat_black_1, alt: "Pants" }],
+    images: [
+      { src: pants_adult_1, alt: "Pants Adult 1" },
+      { src: pants_adult_2, alt: "Pants Adult 2" },
+      { src: pants_adult_3, alt: "Pants Adult 3" },
+      { src: pants_adult_4, alt: "Pants Adult 4" },
+    ],
     variants: [],
     details: [
       "shop.pants.details.1",
@@ -172,7 +219,12 @@ export const shopItems = [
     availability: "available",
     tags: ["apparel", "sets", "crop-set-shorts"],
     slug: "crop-set-shorts",
-    images: [{ src: mat_black_1, alt: "Crop Set Shorts" }],
+    images: [
+      { src: crop_set_shorts_1, alt: "Crop Set Shorts 1" },
+      { src: crop_set_shorts_2, alt: "Crop Set Shorts 2" },
+      { src: crop_set_shorts_3, alt: "Crop Set Shorts 3" },
+      { src: crop_set_shorts_4, alt: "Crop Set Shorts 4" },
+    ],
     variants: [],
     details: [
       "shop.crop-set-shorts.details.1",
@@ -181,61 +233,61 @@ export const shopItems = [
       "shop.crop-set-shorts.details.4",
     ],
   },
-  {
-    id: "7",
-    category: "crop-set-skort",
-    title: "shop.crop-set-skort.title",
-    price: "1600 le",
-    description: "shop.crop-set-skort.description",
-    type: "apparel",
-    availability: "available",
-    tags: ["apparel", "sets", "crop-set-skort"],
-    slug: "crop-set-skort",
-    images: [{ src: mat_black_1, alt: "Crop Set Skort" }],
-    variants: [],
-    details: [
-      "shop.crop-set-skort.details.1",
-      "shop.crop-set-skort.details.2",
-      "shop.crop-set-skort.details.3",
-      "shop.crop-set-skort.details.4",
-    ],
-  },
-  {
-    id: "8",
-    category: "training-leotards",
-    title: "shop.training-leotards.title",
-    price: "1400 le",
-    description: "shop.training-leotards.description",
-    type: "apparel",
-    availability: "available",
-    tags: ["apparel", "leotards"],
-    slug: "training-leotard",
-    images: [{ src: mat_black_1, alt: "Training Leotard" }],
-    variants: [],
-    details: [
-      "shop.training-leotards.details.1",
-      "shop.training-leotards.details.2",
-      "shop.training-leotards.details.3",
-      "shop.training-leotards.details.4",
-    ],
-  },
-  {
-    id: "9",
-    category: "jackets",
-    title: "shop.jackets.title",
-    price: "2000 le",
-    description: "shop.jackets.description",
-    type: "apparel",
-    availability: "available",
-    tags: ["apparel", "jackets"],
-    slug: "flip-team-jacket",
-    images: [{ src: mat_black_1, alt: "Jacket" }],
-    variants: [],
-    details: [
-      "shop.jackets.details.1",
-      "shop.jackets.details.2",
-      "shop.jackets.details.3",
-      "shop.jackets.details.4",
-    ],
-  },
+  // {
+  //   id: "7",
+  //   category: "crop-set-skort",
+  //   title: "shop.crop-set-skort.title",
+  //   price: "1600 le",
+  //   description: "shop.crop-set-skort.description",
+  //   type: "apparel",
+  //   availability: "available",
+  //   tags: ["apparel", "sets", "crop-set-skort"],
+  //   slug: "crop-set-skort",
+  //   images: [{ src: mat_black_1, alt: "Crop Set Skort" }],
+  //   variants: [],
+  //   details: [
+  //     "shop.crop-set-skort.details.1",
+  //     "shop.crop-set-skort.details.2",
+  //     "shop.crop-set-skort.details.3",
+  //     "shop.crop-set-skort.details.4",
+  //   ],
+  // },
+  // {
+  //   id: "8",
+  //   category: "training-leotards",
+  //   title: "shop.training-leotards.title",
+  //   price: "1400 le",
+  //   description: "shop.training-leotards.description",
+  //   type: "apparel",
+  //   availability: "available",
+  //   tags: ["apparel", "leotards"],
+  //   slug: "training-leotard",
+  //   images: [{ src: mat_black_1, alt: "Training Leotard" }],
+  //   variants: [],
+  //   details: [
+  //     "shop.training-leotards.details.1",
+  //     "shop.training-leotards.details.2",
+  //     "shop.training-leotards.details.3",
+  //     "shop.training-leotards.details.4",
+  //   ],
+  // },
+  // {
+  //   id: "9",
+  //   category: "jackets",
+  //   title: "shop.jackets.title",
+  //   price: "2000 le",
+  //   description: "shop.jackets.description",
+  //   type: "apparel",
+  //   availability: "available",
+  //   tags: ["apparel", "jackets"],
+  //   slug: "flip-team-jacket",
+  //   images: [{ src: mat_black_1, alt: "Jacket" }],
+  //   variants: [],
+  //   details: [
+  //     "shop.jackets.details.1",
+  //     "shop.jackets.details.2",
+  //     "shop.jackets.details.3",
+  //     "shop.jackets.details.4",
+  //   ],
+  // },
 ];
